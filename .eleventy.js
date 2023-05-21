c75b9f3add7dabc7ab1897dd71c3891e5c4be92d
Claudia R <claudia.reynders@protonmail.com>
2023-05-21 15:31:01 +0200

    Added basic eleventy setup and index page structure

---
+++ .eleventy.js
@@ -0,0 +1,51 @@
+const eleventyGoogleFonts = require('eleventy-google-fonts')
+const Image = require('@11ty/eleventy-img')
+
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPlugin(eleventyGoogleFonts)
+
+  /*===================================================*/
+  /* files that need to be copied to the build folder  */
+  /*===================================================*/
+  eleventyConfig.addPassthroughCopy('./src/assets/images')
+  eleventyConfig.addPassthroughCopy('./src/assets/icons')
+  eleventyConfig.addPassthroughCopy('./src/assets/sprite.svg')
+  eleventyConfig.addPassthroughCopy({
+      'node_modules/svg-icon-sprite/dist/svg-icon-sprite.js': 'assets/svg-icon-sprite.js'
+  })
+
+  const imageShortcode = async (imageObj = {}) => {
+    const widths = imageObj.widths || [300, 600, 900, 1200]
+    const className = imageObj.className || "image"
+  
+    const sizes = "(min-width: 100px) 50vw, 100vw"
+    const metadata =  await Image(imageObj.src, {
+        formats: ["webp", "jpeg"],
+        outputDir: "./_site/assets/images/generated/",
+        urlPath: "/assets/images/generated/",
+        widths: widths
+    })
+    const alt = imageObj.alt;
+  
+    const imageAttributes = {
+        class: className,
+        alt,
+        sizes,
+        loading: "lazy",
+        decoding: "async",
+    }
+  
+    return Image.generateHTML(metadata, imageAttributes)
+  }
+
+  eleventyConfig.addAsyncShortcode('image', imageShortcode)
+
+  return {
+    dir: {
+      input: 'src',
+      output: '_site',
+    },
+    markdownTemplateEngine: 'njk'
+  }
+
+}
